efeecb7578a86f55bec15ea36bc41834b4f7bb4f
wisniad <wisniad@gmail.com>
2018-06-08 20:59:06 +0200

    - Setting style

--- src/AppVue.vue
+++ src/AppVue.vue
@@ -1,43 +1,54 @@
 <template>
   <div id="app">
-    <div class="container">
-    <table>
-      <thead>
-      <tr>
-        <th><b-btn  class="btn btn-info">Index</b-btn></th>
-        <th><b-btn  class="btn btn-info">Imie</b-btn></th>
-        <th><b-btn  class="btn btn-info">Nazwisko</b-btn></th>
-        <th>
-          <b-input-group>
-            <b-dropdown text="Dropdown" variant="success" slot="append">
-              <b-dropdown-item @click="forwardFilter('')">Wszystkie</b-dropdown-item>
-              <b-dropdown-item @click="forwardFilter('yes')">Z przekierowaiem</b-dropdown-item>
-              <b-dropdown-item @click="forwardFilter('no')">Brak przekierowania</b-dropdown-item>
-            </b-dropdown>
-          </b-input-group>
-        </th>
-        <th @click="sort('number')" ><b-btn  class="btn btn-success sortClick">Number</b-btn></th>
-        <th @click="sort('dodano')" ><b-btn  class="btn btn-success sortClick">Dodano</b-btn></th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-for="(record, indexRecord) in sortedRecords">
-        <td>{{indexRecord+1}}</td>
-        <td>{{record.imie}}</td>
-        <td>{{record.nazwisko}}</td>
-        <td>{{record.forward}}</td>
-        <td>{{record.number}}</td>
-        <td>{{record.dodano}}</td>
-      </tr>
-      </tbody>
-    </table>
-
-  </div>
+    <b-container class="bv-example-row text-center">
+      <b-row align-h="center">
+        <h1 class="paddingContent">Call-sort Vue App</h1>
+          <table class="table table-striped">
+            <thead class="thead-dark">
+            <tr>
+              <th >
+                <b-btn class="btn btn-info">Index</b-btn>
+              </th>
+              <th>
+                <b-btn class="btn btn-info">Imie</b-btn>
+              </th>
+              <th>
+                <b-btn class="btn btn-info">Nazwisko</b-btn>
+              </th>
+              <th>
+                <b-input-group class="centerCenter">
+                  <b-dropdown text="Przekierowania" variant="success" slot="append">
+                    <b-dropdown-item @click="forwardFilter('')">Wszystkie</b-dropdown-item>
+                    <b-dropdown-item @click="forwardFilter('yes')">Z przekierowaiem</b-dropdown-item>
+                    <b-dropdown-item @click="forwardFilter('no')">Brak przekierowania</b-dropdown-item>
+                  </b-dropdown>
+                </b-input-group>
+              </th>
+              <th @click="sort('number')">
+                <b-btn class="btn btn-success sortClick">Number</b-btn>
+              </th>
+              <th @click="sort('dodano')">
+                <b-btn class="btn btn-success sortClick">Dodano</b-btn>
+              </th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="(record, indexRecord) in sortedRecords">
+              <td>{{indexRecord+1}}</td>
+              <td>{{record.imie}}</td>
+              <td>{{record.nazwisko}}</td>
+              <td>{{record.forward}}</td>
+              <td>{{record.number}}</td>
+              <td>{{record.dodano}}</td>
+            </tr>
+            </tbody>
+          </table>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-
   export default {
     name: 'App',
     components: {},
@@ -54,37 +65,35 @@
       fetch('http://esense2.voicetel.pl:20005/getResults/?firstName=voicetel&lastName=test')
         .then(res => res.json())
         .then(res => {
-          // console.log(res)
           this.records = res;
         })
     },
-    methods:{
-      sort:function(s) {
-        //if s == current sort, reverse
-        if(s === this.currentSort) {
-          this.currentSortDir = this.currentSortDir==='asc'?'desc':'asc';
+    methods: {
+      sort: function (s) {
+        if (s === this.currentSort) {
+          this.currentSortDir = this.currentSortDir === 'asc' ? 'desc' : 'asc';
         }
         this.currentSort = s;
       },
-      forwardFilter:function (s) {
+      forwardFilter: function (s) {
         this.forward = s;
       }
 
     },
-    computed:{
-      sortedRecords:function() {
-        return this.records.sort((a,b) => {
+    computed: {
+      sortedRecords: function () {
+        return this.records.sort((a, b) => {
           let modifier = 1;
-          if(this.currentSortDir === 'desc') modifier = -1;
-          if(a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
-          if(a[this.currentSort] > b[this.currentSort]) return 1 * modifier;
+          if (this.currentSortDir === 'desc') modifier = -1;
+          if (a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
+          if (a[this.currentSort] > b[this.currentSort]) return 1 * modifier;
           return 0;
-        }).filter( a  => {
-          if(this.forward === ''){
+        }).filter(a => {
+          if (this.forward === '') {
             return a;
           }
           else {
-           return a['forward'] === this.forward;
+            return a['forward'] === this.forward;
           }
         })
       }
@@ -95,9 +104,28 @@
 <style>
   @import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
+  h1 {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 800;
+    letter-spacing: 2px;
+  }
   td, th {
     padding: 5px;
   }
 
-
+  table {
+    border-left: #3f3f3f solid 1px;
+    -webkit-box-shadow: 4px 4px 5px 1px rgba(0,0,0,0.75);
+    -moz-box-shadow: 4px 4px 5px 1px rgba(0,0,0,0.75);
+    box-shadow: 4px 4px 5px 1px rgba(0,0,0,0.75);
+  }
+  .paddingContent {
+    padding: 1% 0 1% 0;
+  }
+  .centerCenter {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
